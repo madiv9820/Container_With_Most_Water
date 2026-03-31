0970cc710bd7fecaa8d805f0bd3df8f179e25fd3
Manish Mathur <madiv@Manishs-MacBook-Pro.local>
2026-03-31 15:25:01 +0530

    feat(cpp): add initial C++ skeleton💀 solution for Container with Most Water 🪣

---
+++ source/cpp/solution.hpp
@@ -0,0 +1,14 @@
+#define SOLUTION_HPP
+#ifdef SOLUTION_HPP
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        return 0;
+    }
+};
+
+#endif
